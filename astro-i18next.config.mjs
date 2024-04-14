9ed402589c05e375ef0442dcec675b7c1e80f0a2
suku-kahanamoku <sukusovi@gmail.com>
2024-04-14 22:57:18 +0200

    Add new pages.

--- astro-i18next.config.mjs
+++ astro-i18next.config.mjs
@@ -2,4 +2,18 @@
 export default {
 	defaultLocale: 'cs',
 	locales: ['cs', 'en', 'de'],
+	routes: {
+		en: {
+			poradenstvi: 'consultancy',
+			'danova-evidence': 'tax-records',
+			'mzdova-personalni-agenda': 'payroll-agenda',
+			'podvojne-ucetnictvi': 'double-entry-bookkeeping',
+		},
+		de: {
+			poradenstvi: 'beratung',
+			'danova-evidence': 'steuerunterlagen',
+			'mzdova-personalni-agenda': 'lohn-genda',
+			'podvojne-ucetnictvi': 'doppelte-buchfuhrung',
+		},
+	},
 };
